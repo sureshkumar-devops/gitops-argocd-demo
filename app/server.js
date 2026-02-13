27bc229695ca07d7c6b387df4ae31f2920b7a5bf
suresh <suresh@w3logics.com>
2026-02-13 19:13:15 +0000

    added new index file

--- app/server.js
+++ app/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("GitOps + ArgoCD Image Updater Demo 🚀");
+  //res.send("GitOps + ArgoCD Image Updater Demo 🚀");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(3000, () => {
